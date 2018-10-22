3d57a498272e153417326d0b276e19f78cf786cb
Niccolò Zagli <n.zagli18@imperial.ac.uk>
2018-10-22 18:06:53 +0100

    Definition of function main and linearAdvect

--- main_linear_advection.py
+++ main_linear_advection.py
@@ -1,2 +1,27 @@
 #Starting to write code in python
-print()
+def main():
+    "Setting up the parameters of the integration and the fluid"
+    "Integrating the linear advection equation, starting from a given"
+    "initial condition"
+    parameters={'xmin': 0 , 'xmax' : 2, 'dx' : 4*10**-4,
+                'tmin': 0 , 'tmax' : 3, 'dt' : 2*10**-4,
+                'fluid_velocity' : 0.9}
+    linearAdvect(parameters)
+
+def linearAdvect(parameters):
+    xmin = parameters['xmin']
+    xmax = parameters['xmax']
+    tmin = parameters['tmin']
+    tmax = parameters['tmax']
+    dx = parameters['dx']
+    dt = parameters['dt']
+    u = parameters['fluid_velocity']
+    #Derived parameters
+    c = u/(dx/dt)
+    print('The value of the Courant number is %f',c)
+    x = np.arange(xmin,xmax,dx)
+    t = np.arange(tmin,tmax,dt)
+
+
+#Calling the main function
+main()
